e0b258c75dbfd1d1d05bbf24a5c610476ddd4871
power-f-GOD <sundaypower9@gmail.com>
2020-12-24 18:10:35 +0100

    First commit.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {} from 'react-router-dom';
+
+import Box from '@material-ui/core/Box';
+
+const App = () => {
+  return (
+    <Box className='App fade-in' position='relative'>
+      <h1>Mega Online Crusade</h1>
+    </Box>
+  );
+};
+
+export default App;
